d69c62b95fb594a948fa2005728cef25263fdf96
maria2580 <marin6670@gmail.com>
2022-08-21 22:15:34 +0900

    Initial commit

---
+++ src/main/java/com/primitive/rentable_DB_api/RentableDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.primitive.rentable_DB_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentableDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentableDbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primitive/rentable_DB_api/RentableDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primitive.rentable_DB_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentableDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
