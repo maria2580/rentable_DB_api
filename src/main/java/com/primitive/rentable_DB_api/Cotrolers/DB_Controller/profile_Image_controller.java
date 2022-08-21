bc242ea0cce9487a17d254f349101ebc9974f181
maria2580 <marin6670@gmail.com>
2022-08-22 08:41:20 +0900

    프로필 이미지 포스트 할 때 리퀘스트 바디에서 base64 인코딩된 파일을 받도록 수정

--- src/main/java/com/primitive/rentable_DB_api/Cotrolers/DB_Controller/profile_Image_controller.java
+++ src/main/java/com/primitive/rentable_DB_api/Cotrolers/DB_Controller/profile_Image_controller.java
@@ -1,18 +1,18 @@
 package com.primitive.rentable_DB_api.Cotrolers.DB_Controller;
 
-import com.primitive.rentable_DB_api.Srvieces.Item_image_service;
 import com.primitive.rentable_DB_api.Srvieces.Profile_image_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.io.File;
 import java.io.IOException;
+import java.util.Base64;
 
-public class Image_controller {
+public class profile_Image_controller {
     @Autowired
     Profile_image_service profile_image_service;
-
 
 
     @GetMapping( value = "images/profile/{user_id}", produces = MediaType.IMAGE_JPEG_VALUE )
@@ -20,7 +20,11 @@ public class Image_controller {
         return profile_image_service.get_profile_image(user_id);
     }
     @PostMapping("add/images/profile/{user_id}")//@modelattribute("key")
-    public void set_profile_image(@RequestParam String user_id, @RequestParam MultipartFile file){
+    public void set_profile_image(@RequestParam String user_id, @RequestBody String encoded_image) throws IOException {
+        MultipartFile file = null;
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] bytes = decoder.decode(encoded_image);
+        FileCopyUtils.copy(bytes, (File) file);//Todo 체크 안해봄 안될 가능 성 있는 코드임
         new Thread(){
             @Override
             public void run() {
