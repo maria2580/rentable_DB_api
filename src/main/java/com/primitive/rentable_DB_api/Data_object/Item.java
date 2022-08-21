919d7216abb450ffd19ee251e94cb44335aa2ecc
maria2580 <marin6670@gmail.com>
2022-08-22 08:20:48 +0900

    아이템 객체에 인코디드 이미지 배열 추가 내부 코드상에서 데이터 이동을 일관성 있게함. 이미지는 디코드 해서 로컬폴더에 저장

--- src/main/java/com/primitive/rentable_DB_api/Data_object/Item.java
+++ src/main/java/com/primitive/rentable_DB_api/Data_object/Item.java
@@ -2,7 +2,7 @@ package com.primitive.rentable_DB_api.Data_object;
 
 public class Item {
     private int my_index;
-    private String owners_id;
+    private String owners_user_id;
     private String title;
     private String content;
     private String uploaded_date_time;
@@ -10,6 +10,15 @@ public class Item {
     private String reservation_table_name;
     private String comments_table_name;
     private String review_table_name;
+    private String[] encodedImages;
+
+    public String[] getEncodedImages() {
+        return encodedImages;
+    }
+
+    public void setEncodedImages(String[] encodedImages) {
+        this.encodedImages = encodedImages;
+    }
 
     public int getMy_index() {
         return my_index;
@@ -19,12 +28,12 @@ public class Item {
         this.my_index = my_index;
     }
 
-    public String getOwners_id() {
-        return owners_id;
+    public String getOwners_user_id() {
+        return owners_user_id;
     }
 
-    public void setOwners_id(String owners_id) {
-        this.owners_id = owners_id;
+    public void setOwners_user_id(String owners_user_id) {
+        this.owners_user_id = owners_user_id;
     }
 
     public String getTitle() {
